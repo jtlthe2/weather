3d81425befc0b073b2f8a796cfa1f6ac0e140e92
Jamie <jamie.lewis@legitapps.com>
2021-11-29 20:39:26 -0500

    Added tailwind.css

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,20 @@
 import { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import WeatherForCity from './WeatherForCity'
 
 function App() {
 
   return (
     <div>
-      <header>
-        <p>jamie weather</p>
+      <header className={"flex justify-center"}>
+        <h1>jamie weather</h1>
+        <p>username</p>
+        <p>hamburger menu</p>
       </header>
+      <WeatherForCity></WeatherForCity>
+      <WeatherForCity></WeatherForCity>
+      <WeatherForCity></WeatherForCity>
     </div>
   )
 }
