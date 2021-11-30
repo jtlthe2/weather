45537026a6229f1c1ab1b3f9981158b12a575ec8
Jamie <jamie.lewis@legitapps.com>
2021-11-29 20:18:39 -0500

    init
    I initialized a react project using vite.

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import logo from './logo.svg'
+import './App.css'
+
+function App() {
+
+  return (
+    <div>
+      <header>
+        <p>jamie weather</p>
+      </header>
+    </div>
+  )
+}
+
+export default App
